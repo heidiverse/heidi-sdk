8191659a50cb312fdc96e46bbf5d236945437ee2
Alexey Lebedenko <lebedenko@ubique.ch>
2026-01-13 14:07:51 +0100

    Fix future

--- heidi-credentials/rust/src/ldp.rs
+++ heidi-credentials/rust/src/ldp.rs
@@ -2,6 +2,8 @@ use std::{fmt::Display, pin::Pin, sync::Arc};
 
 use heidi_util_rust::value::Value;
 use json_ld::{ChainLoader, ReqwestLoader};
+use static_iref::iri;
+use tokio::task::JoinError;
 
 use crate::{
     claims_pointer::Selector,
@@ -10,7 +12,7 @@ use crate::{
 
 #[derive(Debug, Clone, uniffi::Error)]
 pub enum ParseError {
-    Stuff,
+    JoinError(String),
 }
 
 impl Display for ParseError {
@@ -36,26 +38,43 @@ impl LdpVC {
 
 const CONTEXT_W3C: &'static str = include_str!("../jsonld/www.w3.org/ns/credentials/v2");
 
-fn test(credential: String) -> Pin<Box<dyn Future<Output = ()> + Send>> {
-    Box::pin(async move {
-        let loader = ChainLoader::new(
-            StaticLoader::new().with_document("https://www.w3.org/ns/credentials/v2", CONTEXT_W3C),
-            ReqwestLoader::new(),
-        );
-
-        // credential is now safely owned inside this async block
-        let flattened = JsonLdDocument::new(credential.as_str(), loader)
-            .flattened()
-            .await;
-
-        println!("FLATTENED: {}", flattened);
-    })
-}
-
 #[cfg_attr(feature = "uniffi", uniffi::export(async_runtime = "tokio"))]
 pub async fn parse_ldp_vc(credential: String) -> Result<LdpVC, ParseError> {
-    // Just await the function directly
-    test(credential).await;
+    let vc = tokio::task::spawn_blocking(move || {
+        let handle = tokio::runtime::Handle::current();
+        let local = tokio::task::LocalSet::new();
+
+        // Use the handle to block, and let the local set run the future
+        handle.block_on(local.run_until(async move {
+            let loader = ChainLoader::new(
+                StaticLoader::new()
+                    .with_document("https://www.w3.org/ns/credentials/v2", CONTEXT_W3C),
+                ReqwestLoader::new(),
+            );
+            let context = vec![
+                iri!("https://www.w3.org/ns/credentials/v2").to_owned(),
+                iri!("https://www.w3.org/ns/credentials/examples/v2").to_owned(),
+            ];
+
+            let flattened = JsonLdDocument::new(credential.as_str(), &loader)
+                .flattened()
+                .await;
+            let flattened = JsonLdDocument::new(&flattened.to_string(), &loader);
+
+            let frame = serde_json::json!({
+                "@type": "https://www.w3.org/2018/credentials#VerifiableCredential"
+            });
+            let framed = flattened.framed(&frame).await;
+
+            let document = JsonLdDocument::new(&framed.to_string(), &loader)
+                .compacted(context)
+                .await;
+
+            document
+        }))
+    })
+    .await
+    .map_err(|e| ParseError::JoinError(e.to_string()))?;
 
     todo!()
 }
