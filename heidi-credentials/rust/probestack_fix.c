a92a37f66e006c5b44350c692c035a8bc6dd8194
Alexey Lebedenko <lebedenko@ubique.ch>
2026-01-16 08:57:41 +0100

    probestack fix

---
+++ heidi-credentials/rust/probestack_fix.c
@@ -0,0 +1,12 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
+// This file provides a weak symbol for __rust_probestack to fix linking issues
+// with wasmer 4.3.x on Rust 1.91.x
+//
+// See: https://github.com/rust-lang/rust/issues/142612
+//      https://github.com/rust-lang/rust/issues/143835
+
+__attribute__((weak))
+void __rust_probestack(void) {
+}
